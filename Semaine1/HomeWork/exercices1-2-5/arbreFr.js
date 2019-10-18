e99e4e8605398a8784c8c28f550f7c2afbfcd25a
EL-IDRISSI_MOHAMMED <ingmedical16@gmail.com>
2019-10-18 12:17:35 +0200

    verficatin  de semaine

--- Semaine1/HomeWork/exercices1-2-5/arbreFr.js
+++ Semaine1/HomeWork/exercices1-2-5/arbreFr.js
@@ -1,141 +1,147 @@
 class Noeud {
-    constructor (data) {
-    this.valeur = data
-    this.gauche = null
-    this.droit = null
+    constructor(val) {
+        this.valeur = val;
+        this.gauche = null;
+        this.droite = null;
+        this.parent = null;
+    }
+
+    // Affiche la valeur du noeud et la valeur de ses deux enfants et de son parent
+    toString() {
+        var out = "Noeud " + this.valeur + ":  L";
+
+        this.gauche === null ? out += "-" : out += this.gauche.valeur;
+        out += " R";
+
+        this.droite === null ? out += "-" : out += this.droite.valeur;
+        out += " P";
+
+        this.parent === null ? out += "-" : out += this.parent.valeur;
+        log(out);
     }
 }
-    
-
-class Arbare {
-    constructor(){
-        this.racine = null
-    }
-    // function ajouter data  qui  apple une function ajouter noude
-    ajouter(data){
-        var nouNoeud = new Noeud(data)
-        if(this.racine === null){
-            this.racine = nouNoeud
-        }else{
-            this.ajouterNoeud(this.racine,nouNoeud)
-        }
-    }
-    ajouterNoeud(noeud,nouNoeud){
-        if(nouNoeud.data < noeud.data){
-            if (noeud.gauche === null){
-                noeud.gauche = nouNoeud
-            }else{
-                this.ajouterNoeud(noeud.gauche,nouNoeud)
-            }
-        }else{
-            if(noeud.droit === null){
-                noeud.droit = nouNoeud
-            }else {
-                this.ajouterNoeud(noeud.droit,nouNoeud)
-            }
-        }
+class Arbre {
+    constructor() {
+        this.racine = null;
     }
 
-    supprimer(data){
-        this.racine = this.supprimerNoeud(this.racine,data)
-    }
-    supprimerNoeud(noeud,key){
-        if (noeud ===null){
-            return null
-        }else if(key < noeud.data){
-            noeud.gauche = this.supprimerNoeud(noeud.gauche,key)
-            return noeud
-
-        }else if(key > noeud.data){
-            noeud.droit = this.supprimerNoeud(noeud.droit,key)
-            return noeud
-        }else{
-            if(noeud.gauche === null && noeud.droit === null){
-                noeud = null
-                return noeud
-            }
-            if (noeud.gauche === null){
-                noeud = noeud.droit 
-                return noeud
-
-            }else if(noeud.droit === null){
-                noeud = noeud.gauche 
-                return noeud
-            }
-            aux = this.trouverMinNoeud(noeud.droit)
-            noeud.data = aux.data
-
-            noeud.droit = this.supprimerNoeud(noeud.data,aux.data)
+    //Méthode pour trouver une valeur donnée dans un arbre binaire de recherche
+    trouverNoeud(noeud, valeur) {
+        if (noeud === null) {
+            return null;
+        } else if (valeur < noeud.valeur) {
+            return this.trouverNoeud(noeud.gauche, valeur);
+        } else if (valeur > noeud.valeur) {
+            return this.trouverNoeud(noeud.droite, valeur)
+        } else {
             return noeud
         }
-
     }
 
-    trouverMinNoeud(noeud){
-        if(noeud.gauche === null){
-            return noeud
-        }else {
-            return this.trouverMinNoeud(noeud.gauche)
+    //Méthode pour ajouter un noeud
+    ajouterNoeud(valeur) {
+        var nouNoeud = new Noeud(valeur);
+        if (this.racine === null) {
+            this.racine = nouNoeud;
+        } else {
+            this.insertNoeud(this.racine, nouNoeud)
+        }
+    }
+    insertNoeud(noeud, nouNoeud) {
+        if (nouNoeud.valeur < noeud.valeur) {
+            // if left is null insert node here 
+            if (noeud.gauche === null)
+                noeud.gauche = nouNoeud;
+            else
+
+                // if left is not null recurr until 
+                // null is found 
+                this.insertNoeud(noeud.gauche, nouNoeud);
+        }
+
+        // if the data is more than the node 
+        // data move right of the tree 
+        else {
+            // if right is null insert node here 
+            if (noeud.droite === null)
+                noeud.droite = nouNoeud;
+            else
+
+                // if right is not null recurr until 
+                // null is found 
+                this.insertNoeud(noeud.droite, nouNoeud);
+        }
+    }
+
+
+
+    //Méthode pour supprimer un noeud
+    supprimerNoeud(valeur) {
+        this.racine = this.supNoueud(this.racine, valeur)
+    }
+    supNoueud(noeud, valeur) {
+        if (noeud === null) {
+            return null;
+        } else if (valeur < noeud.valeur) {
+            noeud.gauche = this.supNoueud(noeud.gauche, valeur);
+            return noeud;
+        } else if (valeur < noeud.valeur) {
+            noeud.gauche = this.supNoueud(noeud.gauche, valeur);
+            return noeud;
+        }
+        else {
+            if (noeud.gauche === null && noeud.droite === null) {
+                noeud = null;
+                return noeud;
+            } else if (noeud.gauche === null) {
+                noeud = noeud.droite;
+                return noeud;
+            } else if (noeud.droite === null) {
+                noeud = noeud.gauche;
+                return noeud;
+            }
         }
 
     }
-    getRacineNoeud(){
-        return this.racine
+
+    //Méthode pour afficher l’arbre selon un parcours infixe
+    //Cette méthode doit retournée un tableau contenant la valeur des noeuds
+    infixe() {
+       let  noeud = this.getRacineNoeud();
+       this.enOrdre(noeud);
+       
     }
     enOrdre(noeud){
-        if(noeud !== null){
+        if (noeud !== null) {
             this.enOrdre(noeud.gauche)
-            console.log(noeud.data)
-            this.enOrdre(noeud.droit)
-        }
-
-    }
-
-    preOrdre(noeud){
-        if(noeud !== null){
-        console.log(noeud.data)
-        this.preOrdre(noeud.gauche)
-        this.preOrdre(noeud.droit)
-        }
-
-    }
-
-    postOrdre(noeud){
-        if(noeud !== null){
-            this.postOrdre(noeud.gauche)
-            this.postOrdre(noeud.droit)
-            console.log(noeud.data)
-        }
-    }
-    chercher(noeud,data){
-        if(noeud === null){
-            return null
-        }else if (data < noeud.data){
-            return this.chercher(noeud.gauche,data)
-        }else if (data > noeud.data){
-            return this.chercher(noeud.droit,data)
-        }else {
-            return noeud
-
+            console.log(noeud.valeur)
+            this.enOrdre(noeud.droite)
         }
     }
 
+    
+    getRacineNoeud(){
+        return this.racine
+    
 }
+}
+let a = new Arbre();
+a.ajouterNoeud(30);
+a.ajouterNoeud(18);
+a.ajouterNoeud(24);
+a.ajouterNoeud(11);
+a.ajouterNoeud(33);
+a.ajouterNoeud(13);
+a.ajouterNoeud(40);
+a.ajouterNoeud(46);
+a.ajouterNoeud(14);
+a.ajouterNoeud(21);
+a.ajouterNoeud(12);
+a.ajouterNoeud(10);
+a.ajouterNoeud(31);
+a.ajouterNoeud(35);
+a.ajouterNoeud(32);
 
-
-// cree un objet Arbare
-
-let arb = new Arbare()
-
-arb.ajouter(6)
-arb.ajouter(3)
-arb.ajouter(7)
-arb.ajouter(1)
-arb.ajouter(4)
-arb.ajouter(9)
-
-let racine = arb.getRacineNoeud()
-//console.log(racine)
-arb.enOrdre(racine)
-arb.postOrdre(racine)
-arb.preOrdre(racine)
+console.log(a.infixe());
+var noeud = a.trouverNoeud(24);
+noeud !== undefined ? console.log("La valeur " + noeud.valeur + " appartient à l'arbre.") : console.log("La valeur 24 n'appartient à l'arbre.");
